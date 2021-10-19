08c38c049985802fcf21e52b978fd7483b8165a7
juliaearle <earle.j@northeastern.edu>
2021-10-19 12:34:37 -0400

    initial commmit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//Ciara, Jonah, and Julia
+//Oroville Dam Project
+
+// Rules/Narrative
+
+// Introduction
+// Welcome to our engineering mystery
+// Be prepared to handle the Oroville Dam crisis
+// We want you as the user to decipher the engineering mistake that nearly lead to one of the largest engineering failures ever
+// This program will guide you through the four stages that will allow you to make choices on your path to discovering what went wrong
+// Good luck. And may the odds be ever in your favor.
+
+// Stage 1
+
+// The Oroville Dam is located in Northern California on the Feather River, east of the city Oroville, which is east of Sacramento Valley
+// At 770 feet high, it is the tallest dam in the United States and serves mainly for water supply, hydroelectricity generation, and flood control
+// The rainy season of 2016-2017 was Northern California's wettest winter in over 100 years.
+// Forecast flashes on screen
+//What do you want to do?
+    // (A) Shut-off hydropower
+    // (B) Open normal spillway
+        //Display diagram of dam
+    // (C) Open sluice(s)
+// Heavy rainfall resulted in record inflows from the Feather River, and the spillway was opened in January to relieve the dam pressure
+
+
+// Stage 2
+    // On February 7, Oroville Dam employees noticed an unusual flow pattern
+    // Engineers were called to inspect the integrity of the dam
+    // A large area of concrete and foundation erosion was found, too large to repair
+        //How should the engineers proceed?
+            // (A) Divert the water to the emergency spillway
+            // (B)
+            // (C)
+
+ //Stage 3
+    // After a second series of heavy storms in February, the spillway flow was increased to 50,000 cubic feet per
+    // Second forecast flashes on screen
+        //
+
