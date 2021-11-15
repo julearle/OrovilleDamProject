cc63baf9ab2f653320accf393f1fe224a82ca0a8
julearle <earle.j@northeastern.edu>
2021-11-14 20:30:10 -0500

    added better story

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 using namespace std;
+#include<iostream>
 int main () {
 
-};
+    cout <<"Welcome to our engineering mystery! \n Be prepared to handle the Oroville Dam crisis. We want you as the user to decipher the engineering mistake that nearly lead to one of the largest engineering failures ever. This program will guide you through the four stages that will allow you to make choices on your path to discovering what went wrong. \n Good luck. And may the odds be ever in your favor." << endl;
+    cout<<"The Oroville Dam provides water supply, hydroelectricity generation, and flood control to a region of central California. \n In Febuary 2017, a gaping hole on the dam discovered. This lead to a hasty scramble to figure out a safe solution to fix the hole while combating threats of heavy rainfall and erosion." << endl;
+    cout<<"The Arduino control panel contains four buttons and four corresponding colored LED lights that will be used throughout the game. The correct" << endl;
+//Forecast flashes on screen
+    cout<<"Here is a weather forecast of central California. Circled in black is the location of the Oroville Dam. \n Which option best describes the predicted amount of rainfall in the forecast? Press the red button for 0.01-0.25 inches, yellow for 0.5-2, green for 3-8, or blue for 8-15." << endl;
+//green button is correct answer
+    cout<<"That's correct, the Oroville Dam was inundated with 3-8 inches rain fall. As a result, the water pressure of the dam was record high." << endl;
+
+
+}
